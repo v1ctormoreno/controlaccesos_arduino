62c1d468ac8e31be12984c2876e9ce7a73adf128
Víctor Moreno <victormorenotin@gmail.com>
2020-03-23 20:44:13 +0100

    Added index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+const http = require('http');
+const SerialPort = require('serialport');
+const Readline = require('@serialport/parser-readline');
+const port = new SerialPort('COM7', { baudRate: 9600 });
+const parser = port.pipe(new Readline({ delimiter: '\n' }));
+
+var autorizados = [15683, 214];
+//create a server object:
+http.createServer(function (req, res) {
+    res.write(''); //write a response to the client
+    res.end(''); //end the response
+}).listen(8080); //the server object listens on port 8080
+// Read the port data
+port.on("open", () => {
+    console.log('Serial Open');
+});
+parser.on('data', data => {
+if(autorizados.indexOf(parseInt(data)) !== -1){
+    console.log("aut");
+} else {
+    console.log("no aut");
+    
+}
+});
